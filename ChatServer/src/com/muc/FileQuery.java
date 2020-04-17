accb448f2fdd66d55d2cf4719c064b1b62c6cd94
Lucab2k <gian.luca.beck@gmail.com>
2020-04-17 17:04:02 +0200

    changed filequery

--- ChatServer/src/com/muc/FileQuery.java
+++ ChatServer/src/com/muc/FileQuery.java
@@ -117,7 +117,7 @@ public class FileQuery {
 	
 	
 	//Diese Methode gibt die Namen aller Chats zurück in der sich eine Person befindet. 
-	// Das Übergabeparameter "user" ist case-sensitive!
+	// Das Übergabeparameter "user" ist case-sensitive! //
 	public String[] getAllChatsOfUser(String user) {
 		
 		String path = null;
